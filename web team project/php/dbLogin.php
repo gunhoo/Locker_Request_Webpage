2569966b0402cacf67d9f69e306cd809bf268aca
Unknown <cupjoo@naver.com>
2017-12-03 23:23:15 +0900

    register page

--- web team project/php/dbLogin.php
+++ web team project/php/dbLogin.php
@@ -4,11 +4,9 @@
 	$pw = 'cau1010';
 	$dbName = 'mylocker';
 	$mysqli = new mysqli($host, $user, $pw, $dbName);
-	$myAdmin_id = $_POST['myAdmin_id'];
 
 	$sql = "SELECT * FROM user";
 	$user = $mysqli->query($sql);
-
 	if($user === FALSE) {
 		$sql = "CREATE TABLE user (
 			id VARCHAR(30) NOT NULL,
@@ -16,17 +14,14 @@
 			name VARCHAR(15) NOT NULL,
 			phone_number VARCHAR(15) NOT NULL,
 			email VARCHAR(30) NOT NULL,
-			school VARCHAR(30) NOT NULL,
-			department VARCHAR(30) NOT NULL,
-			major VARCHAR(30) NOT NULL,
 			student_number VARCHAR(12) NOT NULL,
 			PRIMARY KEY (student_number)
 		) ENGINE = InnoDB DEFAULT CHARSET=utf8";
     $user = $mysqli->query($sql);
 	}
+
 	$sql2 = "SELECT * FROM admin";
 	$admin = $mysqli->query($sql2);
-
 	if($admin === FALSE) {
 		$sql2 = "CREATE TABLE admin (
 			id VARCHAR(30) NOT NULL,
@@ -35,8 +30,7 @@
 			phone_number VARCHAR(15) NOT NULL,
 			email VARCHAR(30) NOT NULL,
 			student_number VARCHAR(12) NOT NULL,
-			bank VARCHAR(12),
-			account VARCHAR(20),
+			account VARCHAR(30),
 			PRIMARY KEY (student_number)
 		) ENGINE = InnoDB DEFAULT CHARSET=utf8";
 		$admin = $mysqli->query($sql2);
@@ -44,10 +38,9 @@
 
 	$sql3 = "SELECT * FROM locker";
 	$locker = $mysqli->query($sql3);
-
 	if($locker === FALSE) {
 		$sql3 = "CREATE TABLE locker (
-			locker_id VARCHAR(30) NOT NULL,
+			locker_id INT AUTO_INCREMENT NOT NULL,
 			locker_number INT NOT NULL,
 			building VARCHAR(30) NOT NULL,
 			location VARCHAR(30) NOT NULL,
