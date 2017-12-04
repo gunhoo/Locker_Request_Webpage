5761386e3dc56dbb91bf6c129a8a37c180aee7fe
Unknown <gunhoo2016@naver.com>
2017-12-04 19:15:26 +0900

    user_info_page is changed
    user_info_page is changed

--- web team project/php/user_info_page.php
+++ web team project/php/user_info_page.php
@@ -1,26 +1,27 @@
 <?php
-$host = 'localhost';
-$user = 'root';
-$pw = 'cau1010';
-$dbName = 'mylocker';
-$mysqli = new mysqli($host, $user, $pw, $dbName);
+  $host = 'localhost';
+  $user = 'root';
+  $pw = 'cau1010';
+  $dbName = 'mylocker';
+  $mysqli = new mysqli($host, $user, $pw, $dbName);
+  $myUser_id = $_GET['myUser_id'];
 
   function userInfo(){
-  $sql1 = "UPDATE user SET password = ".$_GET['password']." WHERE id Like 'gunhoo0216'  ";
-  $sql2 = "UPDATE user SET name = ".$_GET['name']." WHERE id Like 'gunhoo0216'";
-  $sql3 = "UPDATE user SET phone_number = ".$_GET['ph_number']." WHERE id LIKE 'gunhoo0216'";
-  $sql4 = "UPDATE user SET email = ".$_GET['email']." WHERE id Like 'gunhoo0216'";
+    $sql1 = "UPDATE user SET password = ".$_GET['password']." WHERE id Like '$myUser_id'  ";
+    $sql2 = "UPDATE user SET name = ".$_GET['name']." WHERE id Like '$myUser_id'";
+    $sql3 = "UPDATE user SET phone_number = ".$_GET['ph_number']." WHERE id LIKE '$myUser_id'";
+    $sql4 = "UPDATE user SET email = ".$_GET['email']." WHERE id Like '$myUser_id'";
 
-  mysqli_query($mysqli, $sql1);
-  mysqli_query($mysqli, $sql2);
-  mysqli_query($mysqli, $sql3);
-  mysqli_query($mysqli, $sql4);
+    mysqli_query($mysqli, $sql1);
+    mysqli_query($mysqli, $sql2);
+    mysqli_query($mysqli, $sql3);
+    mysqli_query($mysqli, $sql4);
 
-}
+  }
 
-$sql = "SELECT* FROM user WHERE id Like 'gunhoo0216'";
-$result = mysqli_query($mysqli, $sql);
-$row = mysqli_fetch_assoc($result);
+  $sql = "SELECT* FROM user WHERE id Like '$myUser_id'";
+  $result = mysqli_query($mysqli, $sql);
+  $row = mysqli_fetch_assoc($result);
 ?>
 
 <head>
@@ -60,8 +61,8 @@ $row = mysqli_fetch_assoc($result);
     <h1>My Locker Info</h1>
     <form>
       <ul id="info_list">
-        <li><input class="info_list" type="text" name="password"  placeholder="Password"></li>
-        <li><input class="info_list" type="text" name="password_confirm"  placeholder="Password Confirm" value="<?php echo $row['password']; ?>"></li>
+        <li><input class="info_list" type="password" name="password"  placeholder="Password" value="<?php echo $row['password'];?>"></li>
+        <li><input class="info_list" type="password" name="password_confirm"  placeholder="Password Confirm" value="<?php echo $row['password'];?>"></li>
         <li><input class="info_list" type="text" name="name" value="<?php echo $row['name'];?>"></li>
         <li><input class="info_list" type="text" name="ph_number" placeholder="Phone Number" value="<?php echo $row['phone_number']; ?>"></li>
         <li><input class="info_list" type="text" name="email" placeholder="E-mail" value="<?php echo $row['email']; ?>"></li>
