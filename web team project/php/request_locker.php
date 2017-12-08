3bd075d16622dbd2dc344daaf489533ec0538fcd
Unknown <gunhoo2016@naver.com>
2017-12-08 14:08:42 +0900

    user request locker page complete
    user request locker page complete

--- web team project/php/request_locker.php
+++ web team project/php/request_locker.php
@@ -6,7 +6,8 @@
   $locker_number = $_POST['locker_number'];
   //USERID검사해서 이미 신청한 DB가 존재하는지 여부 판단
   $d = "SELECT * FROM locker WHERE user_id='$myUser_id'";
-  if($d != NULL)){//존재하지 않으면
+	$checker = $mysqli->query($d);
+  if(!mysqli_fetch_assoc($checker)){//존재하지 않으면
     $sql = "UPDATE locker SET status = 'ready', user_id='$myUser_id'
       WHERE building = '$building' AND location = '$location' AND locker_number = '$locker_number'";
     $mysqli->query($sql);
